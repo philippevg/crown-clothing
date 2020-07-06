593d6f846b6c7fea79d1f1c9a379fd619c2ecac4
Philippe <philippe.van.gucht@hotmail.com>
2020-07-06 18:53:20 +0200

    added Routing with basic Hats page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Switch,  Route } from 'react-router-dom';
 
 import HomePage from "./pages/homepage/homepage.component";
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
+const HastPage = () => (
+    <div>
+      <h1>Hats</h1>
     </div>
-  );
+)
+
+function App() {
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/shop/hats' component={HastPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;

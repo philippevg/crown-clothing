fee67b0185b867907d04b0df799a4c5b9717a0e2
Philippe <philippe.van.gucht@hotmail.com>
2020-07-07 16:59:42 +0200

    added basic shop page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 import { Switch,  Route } from 'react-router-dom';
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HastPage = () => (
-    <div>
-      <h1>Hats</h1>
-    </div>
-)
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
     return (
         <div className="App">
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route path='/shop/hats' component={HastPage} />
+                <Route path='/shop' component={ShopPage} />
             </Switch>
         </div>
     );

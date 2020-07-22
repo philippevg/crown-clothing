0d745801e3b2d5c2fa4a87e351bc443b2a52c30f
Philippe <philippe.van.gucht@hotmail.com>
2020-07-22 22:02:01 +0200

    Before transer of sign in to sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from "redux-persist";
-import thunk from "redux-thunk";
+import createSagaMiddleWare from 'redux-saga';
 
 import combineReducers from './root-reducer';
+import rootSaga from "./root-saga";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleWare();
+
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV === 'development')
     middlewares.push(logger);
 
 export const store = createStore(combineReducers, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga());
 
 export const persistor = persistStore(store);
 
